398a892794dcfcfc57f6904a22a5eb6128bf098b
nevrmore <dev.wsameer@gmail.com>
2020-05-23 20:20:59 +0530

    added a constants file

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const API_BASE_URL_PROD = 'https://uni-search-backend.herokuapp.com/api/universities';
+export const API_BASE_URL_DEV = 'http://localhost:8000/api/universities';
+export const RANDOM_IMAGE = "https://picsum.photos/300/200/";
+export const LIMIT = 6;
+
+
